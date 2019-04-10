3f85855b6d23713d68bc0cbeda894d0b8dbe00d1
伊藤奈保子 <usr4100073@ykpn0017.gmo.local>
2019-04-10 13:49:58 +0900

    Finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+	  test/controllers/users_controller_test.rb
+    assert_response :success
+  end
+
+end
